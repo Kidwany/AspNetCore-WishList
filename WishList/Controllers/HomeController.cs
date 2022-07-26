9f8ef7a65b2d5978aa3b0de69ed709dcb2e13d4d
Mohamed Kidwany <Kidwany@users.noreply.github.com>
2022-07-26 12:34:51 +0200

    Add Controllers and views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
